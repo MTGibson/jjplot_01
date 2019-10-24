34e9c1adecd156e6cd7f8be74919c14835714a9e
mikegibson93 <45637415+mikegibson93@users.noreply.github.com>
2019-10-24 09:44:29 -0400

    Figuring out how to test colors. No luck at the moment mikegibson93 10/24/2019 9:44 AM

--- use_case_histogram.R
+++ use_case_histogram.R
@@ -12,25 +12,27 @@ pacman::p_load('dplyr', 'tidyr', 'gapminder',
                'gmodels', 'beepr','ggmap','maptools','maps')
 
 #J-PAL Colors:
-jpal_orange <- "#E35925"
-jpal_teal <- "#2FAA9F" 
-jpal_green <- "#61b77f"
-jpal_blue <- "#2d616e"
-jpal_yellow <- "#F2C200"
+
 
 test_school_data_fake <- read_excel("test_school_data_fake.xlsx")
 
 
-plot1<-ggplot(data=test_school_data_fake, aes(x=test_score))+
+plotA<-ggplot(data=test_school_data_fake, aes(x=test_score))+
   geom_histogram()+
   labs(title="Distribution of Test Scores",x="Score", y = "Count")
 
-plot2<-plot1+bbc_style()
+plotB<-plotA+bbc_style()
 
-plot3<-plot1+jpal_style()
+plotC<-plotA+jpal_style()
 
 
-plot1
-plot2
-plot3
+plotA
+plotB
+plotC
+
+
+## Testing out colors
+
+
+
 
